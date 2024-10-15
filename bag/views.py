fcbdafc4a7e1cd26db499e05df08f12b9cf8fafc
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-15 17:08:55 +0000

    added update button to bag, 0 removes items

--- bag/views.py
+++ bag/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect, reverse
 
 def view_bag(request):
     """ A view that renders the bag contents page """
-
+     
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
@@ -27,13 +27,13 @@ def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
     bag = request.session.get('bag', {})
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get('quantity', 0))
 
     if quantity > 0:
         bag[item_id] = quantity
     else:
         if item_id in bag:
-            bag.pop(item_id)
+            bag.pop(item_id, None)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
