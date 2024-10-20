e474342ddc5b4112da7ea086df408dc43f8ffe34
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-20 15:40:52 +0000

    added model and view for newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterSubscriptionForm
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+def subscribe(request):
+    if request.method == 'POST':
+        form = NewsletterSubscriptionForm(request.POST)
+        if form.is_valid():
+            # Save the subscription
+            form.save()
+
+            # Send confirmation email
+            email = form.cleaned_data['email']
+            subject = 'Subscription Confirmation'
+            message = 'Thank you for subscribing to our newsletter!'
+            send_mail(
+                subject,
+                message,
+                settings.DEFAULT_FROM_EMAIL,  # Use the email defined in settings
+                [email],
+                fail_silently=False,
+            )
+
+            # Use a success message for toast notification
+            messages.success(request, 'Thank you for subscribing! An email has been sent to confirm your subscription.')
+            return redirect('home')  # Redirect to the home page or another page
+        else:
+            # Use an error message for toast notification
+            messages.error(request, 'Please enter a valid email address.')
+
+    return redirect('home')
--- sw_shop/settings.py
+++ sw_shop/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'storages',
     'wishlistapp',
     'reviews',
+    'newsletter',
 ]
 
 MIDDLEWARE = [
