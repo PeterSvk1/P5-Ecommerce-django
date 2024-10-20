a5f61de0b3275223eb6b95a086b79a029e7d432e
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-20 16:23:15 +0000

    footer is working and added css

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import NewsletterSubscription
+
+@admin.register(NewsletterSubscription)
+class NewsletterSubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at')
+    search_fields = ('email',)
+    ordering = ('-subscribed_at',)
