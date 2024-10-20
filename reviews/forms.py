81c895a23ba3c679e3465b08abb18ed90321bbdb
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-20 00:28:15 +0000

    user can add rationg to his review

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,11 +4,13 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['comment']
+        fields = ['comment','rating']
         widgets = {
             'comment': forms.Textarea(attrs={
                 'rows': 2,
                 'placeholder': 'Write your review here...',
                 'class': 'form-control',
             }),
+            'rating': forms.Select(
+                choices=[(i/10, str(i/10)) for i in range(10, 51, 10)])
         }
