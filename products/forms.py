55df01486cf2e89c7ea8cf53d02f4ccef76b77fc
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-21 16:56:46 +0000

    css for addd product html

--- products/forms.py
+++ products/forms.py
@@ -4,12 +4,12 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-
+    
     class Meta:
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput(attrs={'class': 'custom-file-input'}))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,4 +18,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs['class'] = 'form-control rounded-3'  # Use rounded-3 for a more rounded appearance
