44b7733e43a2917a66ef7861ff4dd76deeaf4dc0
Peter Vlacuha <peter.vlacuha@pobox.sk>
2024-10-21 17:38:58 +0000

    add product page done

--- products/forms.py
+++ products/forms.py
@@ -4,12 +4,19 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-    
+
     class Meta:
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput(attrs={'class': 'custom-file-input'}))
+    image = forms.ImageField(
+        label='Image',
+        required=False,
+        widget=CustomClearableFileInput(attrs={
+            'id': 'new-image',  # Set the ID explicitly
+            'class': 'custom-clearable-file-input'
+        })
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,4 +25,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control rounded-3'  # Use rounded-3 for a more rounded appearance
+            field.widget.attrs['class'] = 'border-black rounded-3' 
